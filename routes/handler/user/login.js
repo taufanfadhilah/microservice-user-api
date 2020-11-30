cf70e1f4b5f70b258253d1a0ac7bac7b465daddf
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2020-11-30 17:02:05 +0700

    change res structure

--- routes/handler/user/login.js
+++ routes/handler/user/login.js
@@ -8,22 +8,16 @@ module.exports = async (req, res) => {
   });
   if (!user || !bcrypt.compareSync(password, user.password)) {
     // condition if user not found or wrong password
-    res.json(
-      {
-        success: false,
-        message: "user not found",
-        data: null,
-      },
-      404
-    );
+    res.status(404).json({
+      success: false,
+      message: "user not found",
+      data: null,
+    });
   }
   delete user.password;
-  res.json(
-    {
-      success: true,
-      message: "login success",
-      data: user,
-    },
-    200
-  );
+  res.status(200).json({
+    success: true,
+    message: "login success",
+    data: user,
+  });
 };
